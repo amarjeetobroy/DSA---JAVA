12574604fdbdd34c2d5f9adac7cb4d7ab0f33742
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-16 10:46:09 +0530

    sum of n natural number

---
+++ D13-Recursion/Natural_number.java
@@ -0,0 +1,20 @@
+// print sum of first n natural number
+
+public class Recursion{
+public static int calcsum(int n){
+		if(n==1){
+			return 1;
+		}
+		int snm1 = calcsum(n-1);
+		int sn = n+snm1;
+		return sn;
+	}
+	public static void main(String[] args) {
+		int n=5;
+		System.out.println(calcsum(n));
+	}
+}
+
+/*Output
+15
+*/
