35c0c51d55badcf9d5d1f49176629d66e117f5e6
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-16 11:22:39 +0530

    String length

---
+++ D10-Strings/string.java
@@ -0,0 +1,12 @@
+// count string length
+
+public class Strings{
+public static void main(String[] args) {
+		String fullname = "Amarjeet kumar";
+		System.out.println(fullname.length());
+	}
+}
+
+/* Output
+14
+*/
