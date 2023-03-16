337225c31f9a4545b1c125570d44a57b9a72781b
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-16 19:53:41 +0530

    last Occurence

---
+++ D13-Recursion/occurence2.java
@@ -0,0 +1,24 @@
+	// write a function to find the last occurence of an element in an array
+
+public class Recursion{
+public static int lastOccurence(int arr[], int key, int i){
+		if(i == arr.length){
+			return -1;
+		}
+		int isFound = lastOccurence(arr, key,i+1);
+		if(isFound == -1 && arr[i] == key){
+			return i;
+		}
+		return isFound;
+	}
+		public static void main(String[] args) {
+			int arr[]= {8,3,6,9,5,6,7,9,2,5,6,7};
+			System.out.println(lastOccurence(arr,5,0));
+		}
+	}
+
+/* Output
+
+9
+
+*/
