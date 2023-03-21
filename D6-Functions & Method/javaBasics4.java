2a676e7df789f9d4e2ee37a3903222e4da32728c
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-22 01:07:47 +0530

    covert from binary to decimal

---
+++ D6-Functions & Method/javaBasics4.java
@@ -0,0 +1,25 @@
+// convert from binary to decimal
+
+public class javaBasics{	
+public static void binToDec(int binNum){
+		int myNum = binNum;
+		int pow =0;
+		int decNum = 0;
+
+		while (binNum > 0){
+			int lastDigit = binNum%10;
+			decNum = decNum + (lastDigit * (int)Math.pow(2,pow));
+			pow ++;
+			binNum = binNum/10;
+		}
+		System.out.println("decimal of " + myNum + " = " +decNum);
+	}
+	public static void main(String[] args) {
+		binToDec(101);
+	}
+}
+
+/* Output
+  decimal of 101 = 5
+  
+  */
