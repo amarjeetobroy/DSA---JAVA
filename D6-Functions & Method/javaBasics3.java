e15b16d60ef6752fd5dd73c811982babcabe014a
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-22 00:41:16 +0530

    check if a number is prime or not

---
+++ D6-Functions & Method/javaBasics3.java
@@ -0,0 +1,24 @@
+// check if a number is prime or not
+
+public class javabasics{	
+public static boolean isprime(int n){
+		boolean isprime = true;
+
+		for(int i=2;i<=n-1;i++){
+			if(n%i == 0){
+				isprime = false;
+				break;
+			}
+		}
+		return isprime;
+	}
+	public static void main(String[] args) {
+		System.out.println(isprime(5));
+	}
+}
+
+/* Output
+
+ true
+ 
+ */
