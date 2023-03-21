4ef09a1d9b5e7756e36e27b249d475763ac7fe51
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-22 00:13:05 +0530

    Find product of a & b

---
+++ D6-Functions & Method/JavaBasics.java
@@ -0,0 +1,19 @@
+// find product of a & b
+
+	public class JavaBasic{
+  public static int multiply(int a, int b){
+		int product = a*b;
+		return product;
+	}
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+
+		int proud = multiply(a,b);
+		System.out.println(proud);
+	}
+}
+
+/* Output
+ 15
+*/
