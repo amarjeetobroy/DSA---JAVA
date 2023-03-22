2c1378f3bffb9030dc5f19239061936f12f90d33
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-03-22 22:41:44 +0530

    update array

---
+++ D7-ARRAYS/Arraycc2.java
@@ -0,0 +1,20 @@
+//passing arrays as arugument
+
+	public static void update(int marks[]){
+		for(int i=0;i<marks.length;i++){
+			marks[i] = marks[i]+1;
+		}
+	}
+	public static void main(String[] args) {
+		int marks[]={96 ,97 ,98};
+		update (marks);
+		for(int i=0;i<marks.length;i++){
+			System.out.print(marks[i]+" ");
+		}
+		System.out.println();
+	}
+}
+
+/* Output
+  97 98 99
+  */
