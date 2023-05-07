8028d8a9fb33c9e4d4fc1924fae5721e4577139e
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-05-07 21:34:15 +0530

    Backtracking - Arrays

---
+++ D16-Backtracking/Backtrack.java
@@ -0,0 +1,30 @@
+public class Backtracking{
+	public static void changeArr(int arr[], int i, int val){
+		//Base case
+		if(i == arr.length){
+			printArr(arr);
+			return;
+		}
+		//recursion
+		arr[i] = val;
+		changeArr(arr,i+1,val+1); // fnx call step
+		arr[i]=arr[i]-2; //Backtracking step
+
+	}
+	public static void printArr(int arr[]){
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		changeArr(arr,0,1);
+		printArr(arr);
+	}
+}
+
+/* Output
+ 1 2 3 4 5
+-1 0 1 2 3
+*/
